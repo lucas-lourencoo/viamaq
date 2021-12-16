118d16a2a4b8b135c88a54aae592031b159d5570
Lucas Lourenço Silva <lucascelestiano@gmail.com>
2021-12-16 16:10:31 -0400

    Catalog Update

--- src/pages/vendas/catalogo.tsx
+++ src/pages/vendas/catalogo.tsx
@@ -1,17 +1,128 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Paginator } from '../../components/Paginator';
 import { Container } from '../../styles/pages/catalog';
 
 const Catalog: NextPage = () => {
+  const [year, setYear] = useState(1990);
+
+  const handleYearRange = (event: any) => {
+    setYear(event.target.value);
+  };
+
   return (
     <Container>
       <Header />
       <Paginator text='Venda | Catálogo' />
 
       <main>
-        <section className='filter'></section>
+        <section className='filter'>
+          <h2>Filtrar por</h2>
+          <form action='' method='POST'>
+            <ul>
+              <li>
+                <h3>Categoria</h3>
+
+                <div className='input'>
+                  <input type='radio' name='category' value='machines' />
+                  <label htmlFor=''>Máquinas</label>
+                </div>
+                <div className='input'>
+                  <input type='radio' name='category' value='pieces' />
+                  <label htmlFor=''>Peças</label>
+                </div>
+              </li>
+              <li>
+                <h3>Marca</h3>
+
+                <div className='input'>
+                  <input type='checkbox' name='category' value='cat' />
+                  <label htmlFor=''>Caterpillar</label>
+                </div>
+                <div className='input'>
+                  <input type='checkbox' name='category' value='john' />
+                  <label htmlFor=''>John Deere</label>
+                </div>
+                <div className='input'>
+                  <input type='checkbox' name='category' value='case' />
+                  <label htmlFor=''>Case</label>
+                </div>
+              </li>
+              <li>
+                <h3>Valor</h3>
+
+                <div className='input'>
+                  <input type='range' name='value' list='tickmarks' />
+                  <datalist id='tickmarks'>
+                    <option value='0' label='0%' />
+                    <option value='10' />
+                    <option value='20' />
+                    <option value='30' />
+                    <option value='40' />
+                    <option value='50' label='50%' />
+                    <option value='60' />
+                    <option value='70' />
+                    <option value='80' />
+                    <option value='90' />
+                    <option value='100' label='100%' />
+                  </datalist>
+                </div>
+              </li>
+              <li>
+                <h3>Ano</h3>
+
+                <div className='inputRange'>
+                  <input
+                    type='range'
+                    name='year'
+                    list='tickmarks'
+                    min='1990'
+                    max='2021'
+                    onChange={handleYearRange}
+                    id='year'
+                  />
+                  <label htmlFor='year'>Ano: 1990 - {year}</label>
+                </div>
+              </li>
+              <li>
+                <h3>Modelo</h3>
+
+                <div className='input'>
+                  <select name='model' id='model'>
+                    <option disabled selected>
+                      Selecione
+                    </option>
+                  </select>
+                </div>
+              </li>
+              <li>
+                <h3>Direção</h3>
+
+                <div className='input'>
+                  <select name='direction' id='direction'>
+                    <option disabled selected>
+                      Selecione
+                    </option>
+                  </select>
+                </div>
+              </li>
+              <li>
+                <h3>Estado</h3>
+
+                <div className='input'>
+                  <select name='state' id='state'>
+                    <option disabled selected>
+                      Selecione
+                    </option>
+                  </select>
+                </div>
+              </li>
+            </ul>
+            <button>Filtrar</button>
+          </form>
+        </section>
         <section className='grid'>
           <div className='item'>
             <img src='/pec1.png' alt='' />
--- src/styles/pages/catalog.ts
+++ src/styles/pages/catalog.ts
@@ -13,6 +13,83 @@ export const Container = styled.main`
     max-width: 1200px;
     margin: 5rem auto;
 
+    .filter {
+      background: #fafaf5;
+      border: 1px solid #e3aa00;
+
+      h2 {
+        text-align: center;
+        background: #e3aa00;
+        color: var(--white);
+        font-size: 1.125rem;
+        text-transform: uppercase;
+        font-weight: 700;
+        padding: 1rem 0;
+      }
+
+      form {
+        padding: 0 0.5rem;
+        margin-top: 1.5rem;
+
+        button {
+          background: var(--blue);
+          color: var(--white);
+          font-size: 1rem;
+          width: 100%;
+          margin-top: 2rem;
+          padding: 0.8rem 0;
+          border-radius: 5px;
+          transition: 0.3s;
+
+          :hover {
+            filter: brightness(90%);
+          }
+        }
+
+        h3 {
+          border-bottom: 2px solid #bababa;
+          padding: 0.25rem 0;
+          margin-top: 1rem;
+        }
+
+        .input {
+          display: flex;
+          align-items: center;
+          margin-top: 0.5rem;
+
+          input[type='radio'],
+          input[type='checkbox'] {
+            width: 16px;
+            height: 16px;
+            margin-right: 0.25rem;
+          }
+
+          select {
+            border: 1px solid #ffd966;
+            box-sizing: border-box;
+            border-radius: 3px;
+            width: 100%;
+            padding: 0.5rem;
+            margin-top: 0.5rem;
+          }
+        }
+
+        .inputRange {
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+
+          input {
+            margin-top: 0.5rem;
+          }
+
+          label {
+            margin-top: 0.5rem;
+          }
+        }
+      }
+    }
+
     .grid {
       display: grid;
       grid-template-columns: repeat(3, 1fr);
