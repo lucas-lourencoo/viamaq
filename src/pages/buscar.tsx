42fbb9c013eb9a2b7088b4b8dad85b64e266693a
Lucas Lourenço Silva <lucascelestiano@gmail.com>
2021-12-28 17:48:26 -0400

    28/12 Update

--- src/pages/buscar.tsx
+++ src/pages/buscar.tsx
@@ -38,24 +38,33 @@ const Search: NextPage = () => {
               <label htmlFor='direction'>Direção</label>
               <select name='direction' id='direction' required>
                 <option selected>Selecione</option>
+                <option value='Hidráulica'>Hidráulica</option>
+                <option value='Comum'>Comum</option>
               </select>
             </div>
             <div className='inputGroup'>
               <label htmlFor='state'>Estado</label>
               <select name='state' id='state' required>
                 <option selected>Selecione</option>
+                <option value='Nova'>Nova</option>
+                <option value='Usado'>Usado</option>
               </select>
             </div>
             <div className='inputGroup'>
               <label htmlFor='transmission'>Transmissão</label>
               <select name='transmission' id='transmission' required>
                 <option selected>Selecione</option>
+                <option value='Manual'>Manual</option>
+                <option value='Automático'>Automático</option>
               </select>
             </div>
             <div className='inputGroup'>
               <label htmlFor='fuel'>Combustível</label>
               <select name='fuel' id='fuel' required>
                 <option selected>Selecione</option>
+                <option value='Diesel'>Diesel</option>
+                <option value='GLT'>GLT</option>
+                <option value='Gasolina'>Gasolina</option>
               </select>
             </div>
             <div className='inputGroup'>
